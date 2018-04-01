4b762f221b97b24a935c2560aca862078b6bea99
arjaynguyen <arjaynguyen123@hotmail.com>
2018-03-31 20:01:29 -0700

    add socket

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+
+var socket = io();
 
 export default class App extends Component {
   render() {
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
-
+const path = require('path');
+const http = require('http');
 
 let app = express();
+let server = http.Server(app);
+let io = require('socket.io').listen(server);
 
-app.use(express.static(__dirname + './../../'));
-app.listen(3000, () => {
-  console.log('Listening to port 3000');
+// static files
+app.use('/build', express.static(path.resolve(__dirname + '/../../build')));
+
+// routing
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/../../index.html'));
+})
+
+io.on('connect', function(socket) {
+  console.log('a user connected: ' + socket.id);
+});
+
+server.listen(3000, () => {
+  console.log('Listening to port 3000d');
 });
 
