d1e8a938219a167481fed5e175a2a26a128f358e
arjaynguyen <arjaynguyen123@hotmail.com>
2018-04-09 15:53:10 -0700

    add basic ui hooks for client to create/join a namespace

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
+import Lobby from './Lobby.jsx';
 
-// var socket = io();
-var socket2 = io('http://localhost:3000/loginLogout');
+var socket = io();
+// var socket2 = io('http://localhost:3000/loginLogout');
 
 export default class App extends Component {
   render() {
     return (
-      <h1>test</h1>
+      <Lobby socket={socket}/>
     );
   }
 }
